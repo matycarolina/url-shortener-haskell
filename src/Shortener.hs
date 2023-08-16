7f0d972905e011fa7f2b87ca0e19816cc2208573
matycarolina <contrerasmarialejandra1@gmail.com>
2023-08-16 09:58:04 -0500

    Add url fomatter

--- src/Shortener.hs
+++ src/Shortener.hs
@@ -1,19 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Shortener where
 
-import Control.Monad.IO.Class (MonadIO(liftIO))
+import Control.Monad.IO.Class (MonadIO (liftIO))
 import Data.Foldable (for_)
 import Data.IORef (modifyIORef, newIORef, readIORef)
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Text (Text)
+import Data.Text (Text, pack, unpack)
 import qualified Data.Text.Lazy as LT
 import Network.HTTP.Types (status404)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
+import Text.RE.Replace
+import Text.RE.TDFA.String
 import Web.Scotty
+
+onlyLetters :: String -> String
+onlyLetters s = replaceAll "" $ s *=~ [re|$([^a-zA-Z])|]
 
 shortener :: IO ()
 shortener = do
@@ -21,23 +27,25 @@ shortener = do
   scotty 3000 $ do
     get "/" $ do
       (_, urls) <- liftIO $ readIORef urlsR
-      html $ renderHtml $
-        H.html $
-          H.body $ do
-            H.h1 "Shortener"
-            H.form H.! A.method "post" H.! A.action "/" $ do
-              H.input H.! A.type_ "text" H.! A.name "url"
-              H.input H.! A.type_ "submit"
-            H.table $
-              for_ (M.toList urls) $ \(i, url) ->
-                H.tr $ do
-                  H.td (H.toHtml i)
-                  H.td (H.text url)
+      html $
+        renderHtml $
+          H.html $
+            H.body $ do
+              H.h1 "Shortener"
+              H.form H.! A.method "post" H.! A.action "/" $ do
+                H.input H.! A.type_ "text" H.! A.name "url"
+                H.input H.! A.type_ "submit"
+              H.table $
+                for_ (M.toList urls) $ \(i, url) ->
+                  H.tr $ do
+                    H.td (H.toHtml i)
+                    H.td (H.text url)
     post "/" $ do
       url <- param "url"
-      liftIO $ modifyIORef urlsR $
-        \(i, urls) ->
-          (i + 1, M.insert i url urls)
+      liftIO $
+        modifyIORef urlsR $
+          \(i, urls) ->
+            (i + 1, M.insert i (pack (onlyLetters (unpack url))) urls)
       redirect "/"
     get "/:n" $ do
       n <- param "n"
