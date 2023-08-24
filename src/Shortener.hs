cb758439bbfc3274687ef1a6ea38862cd2c2166d
Marialejandra Contreras <34869971+matycarolina@users.noreply.github.com>
2023-08-24 08:13:00 -0500

    Add http-types dependency (#1)
    * Add http-types dependency
    
    * Add url fomatter
    
    * Fix URL validator function
    
    * Update error status when input is invalid url

--- src/Shortener.hs
+++ src/Shortener.hs
@@ -2,18 +2,25 @@
 
 module Shortener where
 
-import Control.Monad.IO.Class (MonadIO(liftIO))
+import Control.Monad.IO.Class (MonadIO (liftIO))
 import Data.Foldable (for_)
 import Data.IORef (modifyIORef, newIORef, readIORef)
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Text (Text)
+import Data.Text (Text, unpack)
 import qualified Data.Text.Lazy as LT
-import Network.HTTP.Types (status404)
+import Network.HTTP.Types (status404, status400)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
+import Text.Regex.PCRE
 import Web.Scotty
+
+-- onlyLetters :: String -> String
+-- onlyLetters s = replaceAll "" $ s *=~ [re|$([^a-zA-Z])|]
+
+isValid :: String -> Bool
+isValid url = url =~ ("^(https?):\\/\\/[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)$" :: String)
 
 shortener :: IO ()
 shortener = do
@@ -21,23 +28,28 @@ shortener = do
   scotty 3000 $ do
     get "/" $ do
       (_, urls) <- liftIO $ readIORef urlsR
-      html $ renderHtml $
-        H.html $
-          H.body $ do
-            H.h1 "Shortener"
-            H.form H.! A.method "post" H.! A.action "/" $ do
-              H.input H.! A.type_ "text" H.! A.name "url"
-              H.input H.! A.type_ "submit"
-            H.table $
-              for_ (M.toList urls) $ \(i, url) ->
-                H.tr $ do
-                  H.td (H.toHtml i)
-                  H.td (H.text url)
+      html $
+        renderHtml $
+          H.html $
+            H.body $ do
+              H.h1 "Shortener"
+              H.form H.! A.method "post" H.! A.action "/" $ do
+                H.input H.! A.type_ "text" H.! A.name "url"
+                H.input H.! A.type_ "submit"
+              H.table $
+                for_ (M.toList urls) $ \(i, url) ->
+                  H.tr $ do
+                    H.td (H.toHtml i)
+                    H.td (H.text url)
     post "/" $ do
       url <- param "url"
-      liftIO $ modifyIORef urlsR $
-        \(i, urls) ->
-          (i + 1, M.insert i url urls)
+      ( if isValid (unpack url)
+          then liftIO $
+            modifyIORef urlsR $
+              \(i, urls) ->
+                (i + 1, M.insert i url urls)
+          else raiseStatus status400 "URL not valid"
+        )
       redirect "/"
     get "/:n" $ do
       n <- param "n"
@@ -46,4 +58,4 @@ shortener = do
         Just url ->
           redirect (LT.fromStrict url)
         Nothing ->
-          raiseStatus status404 "not found"
+          raiseStatus status404 "not valid"
